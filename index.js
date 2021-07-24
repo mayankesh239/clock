cfa0ab891d01ce1b907194faa1fa81e51d4eb4ac
mayankesh239 <mayankesh.ss@gmail.com>
2021-07-24 18:34:30 +0530

    modification

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
-let a;
-let time;
-let date;
-const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-setInterval(() => {
+// declaration of the variable
+let a, time, date, m;
+
+//function to get and print the date and time
+myFunction = () => {
     a = new Date();
-    date = a.toLocaleDateString(undefined, options);
-    time = a.getHours() + ':' + a.getMinutes() + ':' + a.getSeconds();
-    document.getElementById('time').innerHTML = time + "<br> on " + date;
-}, 1000);
+    date = check(a.getDate()) + '-' + check(a.getMonth() + 1) + '-' + check(a.getFullYear());
+    time = check(a.getHours()) + ':' + check(a.getMinutes()) + ':' + check(a.getSeconds());
+    document.getElementById('time').innerHTML = time + "<br> Date: " + date;
+}
+
+//This arrow function will add 0 in front of the number if it is smaller than 10
+check = (m) => {
+    if (m < 10) {
+        m = "0" + m;
+    }
+    return m;
+}
+
+//This will make the function myFunction to execute after every 1000ms.
+window.setInterval(myFunction, 1000);
